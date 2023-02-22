02be9493896efc16b6880e949d35c27a22e6f327
rsceth <eaintthethmusoersc@gmail.com>
2023-02-22 23:05:49 +0630

    initial commit

---
+++ myanmar_ai_platform/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='For the purpose of learning together and sharing is caring !',
+    author='Eaint Thet Hmu',
+    license='MIT',
+)
